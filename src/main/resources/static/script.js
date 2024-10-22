26d166ff3b7303f7daad1080918c9a1d9861825d
Barłomiej Kleszcz <b_kleszcz@interia.pl>
2024-10-22 11:21:31 +0200

    fixes in js operation. Not everything works fine but we are starting to work on implementing angular so perfect vanilla js is not necessary for us

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -6,30 +6,33 @@ document.addEventListener('DOMContentLoaded', () => {
     const resultBox = document.getElementById('result');
     const sentenceA1 = document.getElementById('sentenceA1');
     const sentenceHigher = document.getElementById('sentenceHigher');
-    const correctEnglishWord = document.getElementById('translation')
+    const correctEnglishWord = document.getElementById('translation');
 
     let isAnswerSubmitted = false;
 
-    function getRandomPolishWord() {
-        fetch('api/guess/random')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok: ' + response.statusText);
-                }
-                return response.text();
-            })
-            .then(word => {
-                randomPolishWordBox.innerText = word;
-                resultBox.textContent = '';
-                englishWordInput.value = '';
-                correctEnglishWord.innerText = '';
+function getRandomPolishWord() {
+    fetch('api/guess/random')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok: ' + response.statusText);
+            }
+            return response.text();
+        })
+        .then(word => {
+            randomPolishWordBox.innerText = word;
 
-                isAnswerSubmitted = false;
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-            });
-    }
+            resultBox.textContent = '';
+            englishWordInput.value = '';
+            correctEnglishWord.innerText = '';
+            sentenceA1.innerText = '';
+            sentenceHigher.innerText = '';
+
+            isAnswerSubmitted = false;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
 
     function checkTranslation() {
         const polishWord = randomPolishWordBox.textContent;
@@ -54,9 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     resultBox.style.color = 'green';
                     getSentences(englishWord);
                     getCorrectEnglishWord(polishWord);
-
                 } else {
-//                wstaw jak powinno brzmiec poprawnie
                     resultBox.textContent = 'Niepoprawne tłumaczenie. Spróbuj ponownie.';
                     resultBox.style.color = 'red';
                     getCorrectEnglishWord(polishWord);
@@ -69,37 +70,33 @@ document.addEventListener('DOMContentLoaded', () => {
             });
     }
 
-function getSentences(englishWord) {
-    fetch(`get/sentences?englishWord=${encodeURIComponent(englishWord)}`, { // Używamy prawidłowego endpointu
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok: ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(words => {
-        // Upewnij się, że words to tablica z co najmniej dwoma elementami
-        if (Array.isArray(words) && words.length >= 2) { // Dodajemy sprawdzenie
-            const [sentenceA1, sentenceHigher] = words; // Destrukturyzacja tablicy
-            document.getElementById('sentenceA1').innerText = sentenceA1; // Ustawiamy tekst zdania A1
-            document.getElementById('sentenceHigher').innerText = sentenceHigher; // Ustawiamy tekst zdania wyższego poziomu
-        } else {
-            console.error('Otrzymano niewłaściwy format danych:', words);
-        }
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-    });
-}
-
+    function getSentences(englishWord) {
+        fetch(`get/sentences?englishWord=${encodeURIComponent(englishWord)}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok: ' + response.statusText);
+                }
+                return response.json();
+            })
+            .then(words => {
+                if (Array.isArray(words)) {
+                    document.getElementById('sentenceA1').innerText = words[0] || '';
+                    document.getElementById('sentenceHigher').innerText = words[1] || '';
+                } else {
+                    console.error('Otrzymano niewłaściwy format danych:', words);
+                }
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    }
 
     getRandomPolishWord();
-
 
     submitButton.addEventListener('click', checkTranslation);
     nextWordButton.addEventListener('click', getRandomPolishWord);
@@ -114,7 +111,7 @@ function getSentences(englishWord) {
             event.preventDefault();
         }
     });
-});
+
     function getCorrectEnglishWord(polishWord) {
         fetch(`get/correctEnglishWord?polishWord=${encodeURIComponent(polishWord)}`, {
             method: 'GET',
@@ -122,18 +119,19 @@ function getSentences(englishWord) {
                 'Content-Type': 'application/json'
             }
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok: ' + response.statusText);
-            }
-            return response.text(); // Zmienione na text, aby zobaczyć, co dokładnie jest zwracane
-        })
-        .then(word => {
-            console.log('Received response:', word); // Loguj odpowiedź
-            document.getElementById('translation').innerText = word; // Ustaw odpowiedź jako tekst
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-        });
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok: ' + response.statusText);
+                }
+                return response.text(); // Zmienione na text, aby zobaczyć, co dokładnie jest zwracane
+            })
+            .then(word => {
+                console.log('Received response:', word);
+                document.getElementById('translation').innerText = word;
+                getSentences(word);
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
     }
-
+});
